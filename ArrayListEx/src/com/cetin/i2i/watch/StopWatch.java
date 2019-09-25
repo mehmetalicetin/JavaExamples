2673ea760c5faecf1e5800b4d762070e067159cb
mehmetali.cetin <bm.m.alicetin@gmail.com>
2019-09-25 21:53:31 +0300

    StopWatch class added for following the elapsed time in any method or loop.

---
+++ ArrayListEx/src/com/cetin/i2i/watch/StopWatch.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.cetin.i2i.watch;
+
+/**
+ * A class to measure time elapsed.
+ * 
+ * @author 7767
+ *
+ */
+public class StopWatch {
+
+	public static final double NANOS_PER_SEC = 1000000000.0;
+
+	// instance variables
+	private static long startTime; // it keeps start time any process.
+	private static long stopTime; // it keeps stop time any process.
+
+	/**
+	 * This method is taken startTime.
+	 */
+	public static void start() {
+		startTime = System.nanoTime();
+	}
+
+	/**
+	 * This method is taken stopTime.
+	 */
+	public static void stop() {
+		stopTime = System.nanoTime();
+	}
+
+	/**
+	 * elapsed time in seconds.
+	 * 
+	 * @return the time recorded on the stopwatch in seconds
+	 */
+	public static double time() {
+		return (stopTime - startTime) / NANOS_PER_SEC;
+	}
+
+	@Override
+	public String toString() {
+		return "elapsed time: " + time() + " seconds.";
+	}
+
+	/**
+	 * elapsed time in nanoseconds.
+	 * 
+	 * @return the time recorded on the stopwatch in nanoseconds
+	 */
+	public static long timeInNanoseconds() {
+		return (stopTime - startTime);
+	}
+
+}
