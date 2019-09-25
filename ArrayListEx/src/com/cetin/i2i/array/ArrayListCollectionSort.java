503d8aa3122871c1a55a5d23b6d60b590e86b53d
mehmetali.cetin <bm.m.alicetin@gmail.com>
2019-09-25 21:28:44 +0300

    initial commit and ArrayListCollectionSort.java added under com.cetin.i2i.array package.

---
+++ ArrayListEx/src/com/cetin/i2i/array/ArrayListCollectionSort.java
@@ -0,0 +1,42 @@
+
+package com.cetin.i2i.array;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * A class to provide a sample sort of ArrayList. List is sorted by using
+ * Collections.sort(List) method
+ * 
+ * @author mehmetali.cetin
+ *
+ */
+public class ArrayListCollectionSort {
+
+
+	public static void main(String[] args) {
+		List<Integer> numbers = new ArrayList<>();
+		
+		for (int i = 100; i > 0; i--) {
+			numbers.add(i);
+		}
+		
+		toString(numbers); // display numbers.
+		
+		Collections.sort(numbers); // sort numbers.
+		
+		toString(numbers); // display numbers.
+
+	}
+	
+	
+	/**
+	 * Default toString method. Show list.
+	 * @param values
+	 */
+	public static void toString(List<Integer> values) {
+		System.out.println(values);
+	}
+
+}
