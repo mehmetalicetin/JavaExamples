bb6c056db30241a453d363d7ff1e146d07a7fd15
mehmetali.cetin <mehmetali.cetin@i2i-systems.com>
2020-04-11 18:20:08 +0300

    dynamic array

--- ArrayTypeProblemSolution/src/com/cetin/com/cetin/operation/util/Utility.java
+++ ArrayTypeProblemSolution/src/com/cetin/com/cetin/operation/util/Utility.java
@@ -3,45 +3,79 @@ package com.cetin.com.cetin.operation.util;
 import com.cetin.array.operations.*;
 
 public class Utility {
-    static double number =5;
-    public static Double [] arrayStaic;
+    static double number = 5;
+    public static Double[] arrayStaic;
 
     public static void add(Double[] array, double number) {
-       int index = 0;
+        int index = 0;
         if (array != null) {
-          arrayStaic = add2(array,number,index);
+            arrayStaic = add2(array, number, index);
         }
     }
 
-    public static Double[] add2(Double[] array, double number, int index){
-         try {
-             if(array[index] == null){
-                 array[index] = number;
-                 return array;
-             } else {
-                 add2(array, number, ++index);
-             }
-         }catch (ArrayIndexOutOfBoundsException ex){
-             System.out.println("Array is filled Don't push it !!" + array.length);
-         }
+    public static Double[] add2(Double[] array, double number, int index) {
+        try {
+            if (array[array.length - 1] != null) {
+                array = doGrowArrayCapacity(array);
+            }
+            if (array[index] == null) {
+                array[index] = number;
+                return array;
+            } else {
+                add2(array, number, ++index);
+            }
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Array is filled Don't push it !!" + array.length);
+        }
         return array;
     }
 
+    public static Double[] doGrowArrayCapacity(Double[] objArray) {
+        if (objArray != null) {
+            Double[] newArray = new Double[objArray.length * 2];
+            copyOldToNew(objArray, newArray);
+            objArray = newArray;
+        }
+        return objArray;
+    }
 
-    public static void choiceOperation(OperationTypes operationTypes, Double ... numbers) throws Exception {
-        switch (operationTypes){
+    private static void copyOldToNew(Double[] objArray, Double[] newArray) {
+        System.arraycopy(objArray, 0, newArray, 0, objArray.length);
+    }
+
+    public static void main(String[] args) {
+        Double[] a = new Double[2];
+        a[0] = 1.0;
+        add(a, 2.0);
+        add(a, 3.0);
+        add(a, 3.0);
+        add(a, 3.0);
+        add(a, 3.0);
+        add(a, 3.0);
+        add(a, 3.0);
+        add(a, 3.0);
+        add(a, 3.0);
+        add(a, 3.0);
+        add(a, 3.0);
+
+
+    }
+
+
+    public static void choiceOperation(OperationTypes operationTypes, Double... numbers) throws Exception {
+        switch (operationTypes) {
             case ADDITION:
-                process(new Addition(),numbers);
+                process(new Addition(), numbers);
             case SUBTRACTION:
-                process(new Subcraption(),numbers);
+                process(new Subcraption(), numbers);
             case MULTIPLICATION:
-                process(new Multiplication(),numbers);
+                process(new Multiplication(), numbers);
             case DIVISION:
-                process(new Division(),numbers);
+                process(new Division(), numbers);
         }
     }
 
-    public static <T> void process(IOperation<? extends IOperation<T>> operation, Double ... numbers) throws Exception {
+    public static <T> void process(IOperation<? extends IOperation<T>> operation, Double... numbers) throws Exception {
         operation.preOperation(numbers);
         operation.doOperation();
     }
